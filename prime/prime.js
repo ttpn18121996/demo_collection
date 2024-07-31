e52983be10103152c1b476c3c691d2dbdb0b6fa3
ttpn18121996 <ttpn18121996@gmail.com>
2024-07-31 21:42:33 +0700

    Prime

---
+++ prime/prime.js
@@ -0,0 +1,27 @@
+const frm = document.getElementById('frm');
+const frmInput = document.getElementById('frm-input');
+const frmSubmit = document.getElementById('frm-submit');
+const result = document.getElementById('result');
+
+function isPrime(n) {
+  for (let i = n - 1; i >= Math.sqrt(n); i--) {
+    console.log(i);
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+frm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const rs = isPrime(formData.get('n'));
+
+  if (rs) {
+    result.innerText = 'Số nguyên tố';
+  } else {
+    result.innerText = 'Hợp số';
+  }
+});
